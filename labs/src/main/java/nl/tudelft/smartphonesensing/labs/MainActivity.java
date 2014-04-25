4c7fd8906cfdb62d762d5a0c3a9ad3a9160fee8a
David Hoepelman <dhoepelman@gmail.com>
2014-04-25 12:52:42 +0200

    Werkende accelerometer + Android 4.3 minimaal

--- labs/src/main/java/nl/tudelft/smartphonesensing/labs/MainActivity.java
+++ labs/src/main/java/nl/tudelft/smartphonesensing/labs/MainActivity.java
@@ -11,6 +11,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends Activity implements SensorEventListener {
 
     private static final String LOGTAG = "SensorActivity";
@@ -51,25 +56,59 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     protected void onResume() {
         super.onResume();
-        smanager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        smanager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
     }
     public void onPause() {
         super.onPause();
         smanager.unregisterListener(this);
     }
 
+    private double[] currentValues = new double[] {0.0, 0.0, 0.0};
+    private final double filter_modifier = 0.4;
+
+    private final int bufferLength = 5;
+    // Create empty buffers
+    private ArrayList<ArrayList<Float>> storedValues = new ArrayList<ArrayList<Float>>(3);
+
+    private int currentPos = 0;
     public void onSensorChanged(SensorEvent event) {
         // We received new data from one of the sensors
 
+        //Log.d(LOGTAG, "Type: " + event.sensor.getType());
+
+        if(storedValues.size() < 3) {
+            for(int i=0;i<3;i++) {
+                ArrayList<Float> list = new ArrayList<Float>(bufferLength);
+                list.add(0f);
+                list.add(0f);
+                list.add(0f);
+                list.add(0f);
+                list.add(0f);
+
+                storedValues.add(list);
+            }
+        }
+
         switch(event.sensor.getType()){
             case Sensor.TYPE_ACCELEROMETER:
-                Float x = event.values[0];
-                Float y = event.values[1];
-                Float z = event.values[2];
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                for(int i=0;i<3;i++) {
+                    currentValues[i] = filter_modifier * event.values[i] + (1-filter_modifier) * event.values[i];
+                }
+                for(int i=0;i<3;i++) {
+                    storedValues.get(i).set((currentPos++ % 5), event.values[i]);
+                    Collections.sort(storedValues.get(i));
+                }
 
-                ((TextView)findViewById(R.id.ValX)).setText(x.toString());
-                ((TextView)findViewById(R.id.ValY)).setText(y.toString());
-                ((TextView)findViewById(R.id.ValZ)).setText(z.toString());
+                DecimalFormat f = new DecimalFormat("#.##");
+
+                //((TextView) findViewById(R.id.ValX)).setText(f.format(currentValues[0]));
+                //((TextView)findViewById(R.id.ValY)).setText(f.format(currentValues[1]));
+                //((TextView)findViewById(R.id.ValZ)).setText(f.format(currentValues[2]));
+                // Get the median values
+                ((TextView) findViewById(R.id.ValX)).setText(f.format(storedValues.get(0).get(bufferLength/2+1)));
+                ((TextView)findViewById(R.id.ValY)).setText(f.format(storedValues.get(1).get(bufferLength/2+1)));
+                ((TextView)findViewById(R.id.ValZ)).setText(f.format(storedValues.get(2).get(bufferLength/2+1)));
 
                 break;
         }
