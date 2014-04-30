8830e5aa99f20332cf694d80316e9ccf91d5026f
Mark <Mark>
2014-04-30 02:17:16 +0200

    Some code formatting + fix NPE in onSensorChanged

--- app/src/main/java/nl/tudelft/jemoetgaanapp/app/MainActivity.java
+++ app/src/main/java/nl/tudelft/jemoetgaanapp/app/MainActivity.java
@@ -25,6 +25,9 @@ import android.widget.TextView;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+
+import junit.framework.Assert;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks, SensorEventListener {
@@ -48,26 +51,24 @@ public class MainActivity extends ActionBarActivity
         setContentView(R.layout.activity_main);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+            getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
 
         // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+            (DrawerLayout) findViewById(R.id.drawer_layout));
 
         // Set up accelerometer
-        smanager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        smanager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = smanager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+        // Update the main content by replacing fragments
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+            .commit();
     }
 
     public void onSectionAttached(int number) {
@@ -85,12 +86,11 @@ public class MainActivity extends ActionBarActivity
     }
 
     public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
+        final ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,7 +110,7 @@ public class MainActivity extends ActionBarActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        final int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
@@ -132,9 +132,10 @@ public class MainActivity extends ActionBarActivity
          * number.
          */
         public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
+            final Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+
+            final PlaceholderFragment fragment = new PlaceholderFragment();
             fragment.setArguments(args);
             return fragment;
         }
@@ -145,8 +146,8 @@ public class MainActivity extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            final TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
@@ -154,8 +155,7 @@ public class MainActivity extends ActionBarActivity
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+            ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
 
@@ -163,57 +163,77 @@ public class MainActivity extends ActionBarActivity
         super.onResume();
         smanager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
     }
+
     public void onPause() {
         super.onPause();
         smanager.unregisterListener(this);
     }
 
-    private double[] currentValues = new double[] {0.0, 0.0, 0.0};
+//    private double[] currentValues = new double[] {0.0, 0.0, 0.0};
     private final double filter_modifier = 0.4;
 
     private final int bufferLength = 5;
     // Create empty buffers
-    private ArrayList<ArrayList<Float>> storedValues = new ArrayList<ArrayList<Float>>(3);
+    private final List<List<Float>> storedValues = new ArrayList<List<Float>>(3);
 
     private int currentPos = 0;
+
     public void onSensorChanged(SensorEvent event) {
         // We received new data from one of the sensors
 
         //Log.d(LOGTAG, "Type: " + event.sensor.getType());
 
-        if(storedValues.size() < 3) {
-            for(int i=0;i<3;i++) {
-                ArrayList<Float> list = new ArrayList<Float>(bufferLength);
+        if (storedValues.isEmpty()) {
+            for (int i = 0; i < 3; i++) {
+                final List<Float> list = new ArrayList<Float>(bufferLength);
                 list.add(0f);
                 list.add(0f);
                 list.add(0f);
                 list.add(0f);
                 list.add(0f);
-
                 storedValues.add(list);
             }
         }
 
-        switch(event.sensor.getType()){
+        switch (event.sensor.getType()) {
             case Sensor.TYPE_ACCELEROMETER:
             case Sensor.TYPE_LINEAR_ACCELERATION:
-                for(int i=0;i<3;i++) {
-                    currentValues[i] = filter_modifier * event.values[i] + (1-filter_modifier) * event.values[i];
-                }
-                for(int i=0;i<3;i++) {
-                    storedValues.get(i).set((currentPos++ % 5), event.values[i]);
+                // TODO multiply 1 - filter_modifier with event.values[i-1] for i > 0?
+//                for (int i = 0; i < 3; i++) {
+//                    currentValues[i] = filter_modifier * event.values[i] + (1 - filter_modifier) * event.values[i];
+//                }
+                final int currentIndex = currentPos++ % 5;
+                for (int i = 0; i < 3; i++) {
+                    storedValues.get(i).set(currentIndex, event.values[i]);
                     Collections.sort(storedValues.get(i));
                 }
 
-                DecimalFormat f = new DecimalFormat("#.##");
+                final DecimalFormat f = new DecimalFormat("#.##");
 
                 //((TextView) findViewById(R.id.ValX)).setText(f.format(currentValues[0]));
                 //((TextView)findViewById(R.id.ValY)).setText(f.format(currentValues[1]));
                 //((TextView)findViewById(R.id.ValZ)).setText(f.format(currentValues[2]));
+
+                for (int i = 0; i < 3; i++) {
+                    junit.framework.Assert.assertEquals(bufferLength, storedValues.get(i).size());
+                }
+
+                // TODO Is this callback method the best place to get GUI elements?
+                final TextView valueX = (TextView) findViewById(R.id.ValX);
+                final TextView valueY = (TextView) findViewById(R.id.ValY);
+                final TextView valueZ = (TextView) findViewById(R.id.ValZ);
+
+                if (valueX == null || valueY == null || valueZ == null) {
+                    // First keelhaul all the androids, then abandon ship!
+                    return;
+                }
+
                 // Get the median values
-                ((TextView) findViewById(R.id.ValX)).setText(f.format(storedValues.get(0).get(bufferLength/2+1)));
-                ((TextView)findViewById(R.id.ValY)).setText(f.format(storedValues.get(1).get(bufferLength/2+1)));
-                ((TextView)findViewById(R.id.ValZ)).setText(f.format(storedValues.get(2).get(bufferLength/2+1)));
+                // TODO If Java rounds down with integer division than bufferLength/2+1 = 3, but
+                // needs to be 2 (zero-based index)
+                valueX.setText(f.format(storedValues.get(0).get(bufferLength / 2)));
+                valueY.setText(f.format(storedValues.get(1).get(bufferLength / 2)));
+                valueZ.setText(f.format(storedValues.get(2).get(bufferLength / 2)));
 
                 break;
         }
