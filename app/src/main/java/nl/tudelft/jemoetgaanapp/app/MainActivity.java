cc2f78e47d98fddec0ac0f828e4ce200dd9592b5
David Hoepelman <dhoepelman@gmail.com>
2014-05-01 15:29:32 +0200

    App een beetje opgeruimt

--- app/src/main/java/nl/tudelft/jemoetgaanapp/app/MainActivity.java
+++ app/src/main/java/nl/tudelft/jemoetgaanapp/app/MainActivity.java
@@ -111,9 +111,9 @@ public class MainActivity extends ActionBarActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         final int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/nl/tudelft/jemoetgaanapp/app/NavigationDrawerFragment.java
+++ app/src/main/java/nl/tudelft/jemoetgaanapp/app/NavigationDrawerFragment.java
@@ -247,10 +247,12 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
+        /**
+         * Voorbeeld van een item
         if (item.getItemId() == R.id.action_example) {
             Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
-        }
+        }**/
 
         return super.onOptionsItemSelected(item);
     }
