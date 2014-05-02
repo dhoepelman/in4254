be6e28d3f2edaa0fe73a9a8fdc9e37c26bd6af4a
David Hoepelman <dhoepelman@gmail.com>
2014-05-02 12:39:26 +0200

    Added an interface for the classifier

---
+++ app/src/main/java/nl/tudelft/jemoetgaanapp/app/IClassifier.java
@@ -0,0 +1,18 @@
+package nl.tudelft.jemoetgaanapp.app;
+
+/**
+ * Created by David on 2-5-2014.
+ */
+public interface IClassifier {
+    /**
+     * Classify a measurement as an activity
+     */
+    public ACTIVITY classify(Measurement m);
+
+    /**
+     * Add a training point to the classifier
+     * @param a The activity which was performed
+     * @param m The measurement
+     */
+    public void train(ACTIVITY a, Measurement m);
+}
